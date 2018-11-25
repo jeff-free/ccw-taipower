10ff72016edd486ed3529e73e86478a629fd16fc
JFree <jeffrey410469@gmail.com>
2018-11-26 00:20:39 +0800

    - Add distribution by city view

--- app/controllers/distributions_controller.rb
+++ app/controllers/distributions_controller.rb
@@ -1,6 +1,9 @@
 class DistributionsController < ApplicationController
   def index
     @expenditures = Expenditure.all
+    if params[:city]
+      @expenditures_of_city = @expenditures.where(city: params[:city]).page(params[:page]).per(25)
+    end
   end
 
   def show
